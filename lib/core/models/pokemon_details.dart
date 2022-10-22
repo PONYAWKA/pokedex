32b9999e74e023b9a146659b816a94191a88b3cd
PONYAWKA <lololowkiam@gmail.com>
2022-10-22 17:00:11 +0300

    feat: add models

---
+++ lib/core/models/pokemon_details.dart
@@ -0,0 +1,36 @@
+import 'package:pokedex/core/models/types.dart';
+
+class PokemonDetails {
+  PokemonDetails({
+    this.id,
+    this.name,
+    this.height,
+    this.weight,
+    this.baseExperience,
+    this.types
+  });
+
+  factory PokemonDetails.fromJson(Map<String, dynamic> json) => PokemonDetails(
+    id: json['id'] as int?,
+    name: json['name'] as String?,
+    height: json['height'] as int?,
+    weight: json['weight'] as int?,
+    baseExperience: json['base_experience'] as int,
+    types: json['types'] != null
+        ? (json['types'] as List)
+        .map((e) => Types.fromJson(e as Map<String, dynamic>))
+        .toList()
+        : null,
+  );
+
+
+  final int? id;
+  final String? name;
+  final int? height;
+  final int? weight;
+  final int? baseExperience;
+  final List<Types>? types;
+
+  String get getImageUrl =>
+      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/$id.png';
+}
---
+++ lib/core/models/types.dart
@@ -0,0 +1,14 @@
+import 'package:pokedex/core/models/pokemon_type.dart';
+
+class Types {
+  Types({
+    this.type,
+    this.slot,
+  });
+  factory Types.fromJson(Map<String, dynamic> json) => Types(
+    slot: json['slot'] as int?,
+    type: PokemonType.fromJson(json['type'] as Map<String, dynamic>),
+  );
+  final int? slot;
+  final PokemonType? type;
+}

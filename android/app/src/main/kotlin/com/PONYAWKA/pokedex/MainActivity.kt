dcaa5b92306ebf0732b30a4dc6d264ad23d1f5f2
PONYAWKA <lololowkiam@gmail.com>
2022-10-22 16:43:52 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/PONYAWKA/pokedex/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.PONYAWKA.pokedex
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/main.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+import 'package:pokedex/pages/home/view/home.dart';
+
+void main() => const MaterialApp(home: Home());
+
